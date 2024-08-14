9a682478cda1818a0b5ee97050fcb71b14e1f31f
samuel <linhdang.dev96@gmail.com>
2024-08-14 16:32:37 +0700

    Update information

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -8,33 +8,25 @@ export const name = 'Samuel';
 export const lastName = 'Dang';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'Frontend developer with around 4 years of experience in JavaScript, TypeScript, React.js, and Next.js, along with other frontend technologies. I also have a basic understanding of Node.js, databases, and foundational IT concepts like data structures, algorithms, and performance optimization. My expertise lies in building dynamic, responsive, and efficient web applications that deliver a seamless user experience.';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ platform: Platform.GitHub, link: 'https://github.com/kingsley1996' },
 	{
 		platform: Platform.Linkedin,
-		link: 'https://www.linkedin.com/'
-	},
-	{
-		platform: Platform.Twitter,
-		link: 'https://twitter.com/'
-	},
-	{
-		platform: Platform.StackOverflow,
-		link: 'https://stackoverflow.com/'
+		link: 'https://www.linkedin.com/in/linhdangdev96/'
 	},
 	{
 		platform: Platform.Email,
-		link: 'riadh_adrani@hotmail.fr'
+		link: 'linhdang.dev96@gmail.com'
 	},
 	{
 		platform: Platform.Youtube,
-		link: 'https://www.youtube.com'
+		link: 'https://www.youtube.com/@CodeWithSamuel-VN'
 	},
 	{
-		platform: Platform.Facebook,
-		link: 'https://www.facebook.com'
+		platform: Platform.Tiktok,
+		link: 'https://www.tiktok.com/@codewithsamuel96'
 	}
 ];
 
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -7,7 +7,8 @@ export enum Platform {
 	Linkedin = 'linkedin',
 	Email = 'email',
 	Facebook = 'facebook',
-	Youtube = 'youtube'
+	Youtube = 'youtube',
+	Tiktok = 'tiktok'
 }
 
 export type Icon = `i-${string}-${string}`;
